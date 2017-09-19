786cd6c020e1354961be8bfe26c588c08c5a0c0f
krackWingSam <sangwoo21@askstory.com>
2017-09-19 15:29:51 +0900

    First Commit
    project added

---
+++ Chatory/Chatory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chatory
+//
+//  Created by askstory on 2017. 9. 19..
+//  Copyright © 2017년 askstory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chatory/Chatory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chatory
+//
+//  Created by askstory on 2017. 9. 19..
+//  Copyright © 2017년 askstory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
