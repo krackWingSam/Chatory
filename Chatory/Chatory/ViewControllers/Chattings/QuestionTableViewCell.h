6fcb926d8b17d98a3bd3085ff6743ab007ac3399
krackWingSam <sangwoo21@askstory.com>
2017-09-20 10:18:01 +0900

    Question Table View Cell
    cell for questions

---
+++ Chatory/Chatory/ViewControllers/Chattings/QuestionTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  QuestionTableViewCell.h
+//  Chatory
+//
+//  Created by askstory on 2017. 9. 20..
+//  Copyright © 2017년 askstory. All rights reserved.
+//
+
+#define CELL_QUESTION_HEIGHT 368
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionTableViewCell : UITableViewCell
+
+
+@property (nonatomic) NSDictionary *questionDic;
+
+@end
