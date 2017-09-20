4efe395ab53a456d93232787d8c453190dc85d81
krackWingSam <sangwoo21@askstory.com>
2017-09-20 10:17:40 +0900

    Chatting View Controller
    add table view delegate, data sources

--- Chatory/Chatory/ViewControllers/Chattings/ChattingViewController.m
+++ Chatory/Chatory/ViewControllers/Chattings/ChattingViewController.m
@@ -8,7 +8,12 @@
 
 #import "ChattingViewController.h"
 
-@interface ChattingViewController ()
+@interface ChattingViewController () <UITableViewDataSource, UITableViewDelegate> {
+    IBOutlet UIView *view_Navigation;
+    IBOutlet UIView *view_Navigation_Upper;
+    
+    NSMutableArray *array_Contents;
+}
 
 @end
 
@@ -17,11 +22,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    array_Contents = [[NSMutableArray alloc] init];
+    [self initUI];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)initUI {
+    //TODO: initializing view controller's contents
+    
+//    [view_Navigation.layer setMasksToBounds:YES];
+//    [view_Navigation.layer setCornerRadius:20.f];
+    
+    view_Navigation.layer.cornerRadius = 20;
+    view_Navigation.layer.shadowColor = [UIColor blackColor].CGColor;
+    view_Navigation.layer.shadowOffset = CGSizeMake(0, 4.0);
+    view_Navigation.layer.shadowOpacity = 0.05;
+    view_Navigation.layer.shadowRadius = 5.0;
 }
 
 
@@ -30,5 +51,26 @@
     _contentDic = contentDic;
     //TODO: set contents for character, questions, answers or etc
 }
+
+
+#pragma mark - UITableViewDataSource
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"QuestionCell"];
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [array_Contents count];
+}
+
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *dic = [array_Contents objectAtIndex:indexPath.row];
+    
+    return 368.f;
+}
+
 
 @end
